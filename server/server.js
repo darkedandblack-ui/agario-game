038e476ea428fa4a85d7246fec12681b055f513c
Baran Dinler <darkedandblack@gmail.com>
2026-05-23 13:03:28 +0300

    CORS izni eklendi

--- server/server.js
+++ server/server.js
@@ -5,7 +5,13 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: ["https://gencgundem.com", "https://www.gencgundem.com"],
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 app.use(express.static("public"));
 
